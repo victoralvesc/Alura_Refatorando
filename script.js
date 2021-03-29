473379fa2d9437f6cd079495a749ca9c58c8729e
Victor Alves <victor.alvescosta@outlook.com>
2021-03-29 20:52:14 -0300

    corrigindo arquivo script.js

--- script.js
+++ script.js
@@ -41,25 +41,6 @@ function adicionarFilme() {
   }
   
   
-//   function adicionarFilme() {
-  
-//     var campoFilmeFavorito = document.getElementById('filme')
-//     console.log(campoFilmeFavorito)
-    
-//     var filmeFavorito = campoFilmeFavorito.value
-//     if (filmeFavorito.endsWith('.jpg')) {
-//         listarFilmesNaTela(filmeFavorito)
-//     } else {
-//         alert("Imagem inválida")
-//     }
-//     campoFilmeFavorito.value = ""
-// }
-
-// function listarFilmesNaTela(filme) {
-//     var listaFilmes = document.querySelector('#listaFilmes')
-//     var elementoFilme = "<img src=" + filme + ">"
-//     listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
-// }
 
 
   //---------------comentários--------------
